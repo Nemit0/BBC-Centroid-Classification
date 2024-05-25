42683ec0c6b3e9704d710d834598451b8ffffb56
Nemit <123555510+Nemit0@users.noreply.github.com>
2024-05-25 11:41:34 +0900

    Update

--- get_category_embedding.py
+++ get_category_embedding.py
@@ -20,14 +20,15 @@ def main():
     print('initialized')
 
     data_path = os.path.join(get_project_root(), "data")
+    chunk_path = os.path.join(data_path, "chunk")
+    log_path = os.path.join(chunk_path, "log")
     with open(os.path.join(data_path, "category_list.json"), "r") as f:
         categories = json.load(f)
     print('category loaded')
-    _category_dict = {
-        'categories': categories,
-        'embeddings': [None] * len(categories)
-    }
-    # category_df = pl.DataFrame(_category_dict)
+    # _category_dict = {
+    #     'categories': categories,
+    #     'embeddings': [None] * len(categories)
+    # }
 
     def apply_category_embedding(category):
         embedding = get_embedding(category, openai, encoder)
@@ -44,26 +45,33 @@ def main():
     category_chunks.append(categories[chunk_size*(chunk_num-1):])
     # Ensure that the chunk is perfectly divided without any loss
     assert sum([len(chunk) for chunk in category_chunks]) == len(categories)
-
-    # # Process per chunk using multiprocessing
-    # with Pool(cpu_count()) as pool:
-    #     results = pool.map(apply_category_embedding, categories)
-    #     for category, embedding in results:
-    #         idx = categories.index(category)
-    #         _category_dict['embeddings'][idx] = embedding
-    # print('embedding done')
-
+    
+    for i, chunk in enumerate(category_chunks):
+        with open(os.path.join(chunk_path, f"chunk_{i}_categories.json"), "w") as f:
+            json.dump(chunk, f)
+    
+    chunk_list = [os.path.join(chunk_path, f"chunk_{i}_categories.json") for i in range(chunk_num)]
+    chunk_list = [_chunk for _chunk in chunk_list if not os.path.exists(f"{_chunk}_embeddings.json")]
     # Process per chunks
-    for chunk in tqdm(category_chunks):
-        with ThreadPoolExecutor(max_workers=100) as executor:
+    for i, chunk_filename in enumerate(tqdm(chunk_list)):
+        with open(chunk_filename, "r") as f:
+            chunk = json.load(f)
+        # strip chunk_filename to get only filename
+        _chunk_filename = os.path.basename(chunk_filename)
+        _category_dict = {
+                    'categories': chunk,
+                    'embeddings': [None] * len(chunk)
+        }
+        with ThreadPoolExecutor(max_workers=20) as executor:
             futures = [executor.submit(apply_category_embedding, category) for category in chunk]
             for future in as_completed(futures):
                 category, embedding = future.result()
-                # print(category, embedding)
-                # idx = categories.index(category)
-                # print(idx)
-                #category_df[idx, 'embeddings'] = embedding
-                _category_dict['embeddings'][categories.index(category)] = embedding
+                chunk_index = chunk.index(category)
+                _category_dict['embeddings'][chunk_index] = embedding
+        with open(os.path.join(chunk_path, f"{_chunk_filename}_embeddings.json"), "w") as f:
+            json.dump(_category_dict, f)
+        del _category_dict
+
     print('embedding done')
     
     category_df = pl.DataFrame(_category_dict)
