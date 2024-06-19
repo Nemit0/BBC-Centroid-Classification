df8d71bba7ddce09c1ae58cc633ba0e78296ae52
Nemit <123555510+Nemit0@users.noreply.github.com>
2024-06-19 14:38:46 +0900

    Update

--- bbc_analysis.py
+++ bbc_analysis.py
@@ -3,16 +3,19 @@ import json
 import nltk
 import re
 import warnings
+import random
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from module.utils import get_project_root
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from typing import List
+from typing import List, Iterable
 from rich import print
+from tqdm import tqdm
 
 from sklearn.feature_selection import chi2
 
@@ -47,6 +50,9 @@ def gaussian_pdf(x, variance, mean=0):
         variance += 1e-10
     return (1 / np.sqrt(2 * np.pi * variance)) * np.exp(-0.5 * ((x - mean) ** 2) / variance)
 
+def cost(a, b) -> float:
+    return float(len(set(a).difference(b)) + len(set(b).difference(a))/len(set(a).union(b)))
+
 def clean_and_split_words(text: str, use_stemming: bool = False) -> list:
     """
     Clean and split words from text.
@@ -74,15 +80,24 @@ def generate_ngrams(words, ngram_range):
 def normalize_vector(vector: np.ndarray) -> np.ndarray:
     return np.divide(vector, np.linalg.norm(vector))
 
-class TfIdfVectorizer:
+def find_candidates(row, pmf_cols:Iterable, thresholds:dict) -> List[int]:
+    candidates = []
+    for idx, pmf_col in enumerate(pmf_cols):
+        if row[pmf_col] >= thresholds[idx]:  # Check if PMF exceeds the threshold for this category
+            candidates.append(idx)  # Use the category index as candidate
+    return candidates if candidates else [-1]
+
+class TfIdfVectorizer(object):
     def __init__(self, sublinear_tf=True, min_df=5, norm='l2', ngram_range=(1, 2), stop_word_lang='english'):
-        nltk.download('stopwords')
+        # nltk.download('stopwords')
         self.sublinear_tf = sublinear_tf
         self.min_df = min_df
         self.norm = norm
         self.ngram_range = ngram_range
         self.stop_words = stopwords.words(stop_word_lang)
         self.token_map = {}
+        self.tf_value = {}
+        self.idf_value = {}
     
     def fit_transform(self, documents: pd.Series) -> np.ndarray:
         if self.ngram_range == (1,1):
@@ -100,11 +115,12 @@ class TfIdfVectorizer:
 
         total_docs = len(documents)
         idf = {word: np.log(total_docs / (df[word] + 1)) + 1 for word in vocabulary}
-
+        self.idf = idf
         tf = {word: 0 for word in vocabulary}
         for tokens in docs_tokens:
             for token in tokens:
                 tf[token] += 1
+        self.tf = tf
         
         vocab_index = {word: idx for idx, word in enumerate(vocabulary) if tf[word] >= self.min_df}
         vocabulary = set(vocab_index.keys())
@@ -132,9 +148,13 @@ class TfIdfVectorizer:
         self.token_map = vocab_index
 
         return tfidf_matrix
+
+    def embed_doc(self, doc:str) -> np.array:
+        raise NotImplementedError
+        
     
 
-def main():
+def main(*args, **kwargs) -> tuple:
     root_path = os.path.join(get_project_root(), 'data', 'bbc')
     text_path = os.path.join(root_path, 'raw_text')
 
@@ -146,6 +166,9 @@ def main():
         'sport': 3,
         'tech': 4
     }
+
+    inv_class_id_map = {v: k for k, v in class_id_map.items()}
+
     df_dict = {
         'class': [],
         'text': [],
@@ -181,28 +204,28 @@ def main():
     features_norm = vectorizernorm.fit_transform(texts)
     print(f"Shape of the feature matrix: {features.shape}")
 
-    # print(f"Calculating Chi2 for feature-category correlation analysis")
-    # N = 5
-    # for category, category_id in sorted(class_id_map.items()):
-    #     features_chi2 = chi2(features, df['classid'] == category_id)
-    #     indices = np.argsort(features_chi2[0])
-    #     feature_names = np.array(list(vectorizer.token_map.keys()))[indices]
-    #     unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-    #     bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-    #     print("# '{}':".format(category))
-    #     print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
-    #     print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
-    # print(f"Calculating Chi2 for feature-category correlation analysis (Normalized)")
-    # N = 5
-    # for category, category_id in sorted(class_id_map.items()):
-    #     features_chi2 = chi2(features_norm, df['classid'] == category_id)
-    #     indices = np.argsort(features_chi2[0])
-    #     feature_names = np.array(list(vectorizernorm.token_map.keys()))[indices]
-    #     unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-    #     bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-    #     print("# '{}':".format(category))
-    #     print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
-    #     print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
+    print(f"Calculating Chi2 for feature-category correlation analysis")
+    N = 5
+    for category, category_id in sorted(class_id_map.items()):
+        features_chi2 = chi2(features, df['classid'] == category_id)
+        indices = np.argsort(features_chi2[0])
+        feature_names = np.array(list(vectorizer.token_map.keys()))[indices]
+        unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+        bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+        print("# '{}':".format(category))
+        print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
+        print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
+    print(f"Calculating Chi2 for feature-category correlation analysis (Normalized)")
+    N = 5
+    for category, category_id in sorted(class_id_map.items()):
+        features_chi2 = chi2(features_norm, df['classid'] == category_id)
+        indices = np.argsort(features_chi2[0])
+        feature_names = np.array(list(vectorizernorm.token_map.keys()))[indices]
+        unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+        bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+        print("# '{}':".format(category))
+        print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
+        print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
 
     df['embeddings'] = list(features)
     df_norm['embeddings'] = list(features_norm)
@@ -216,8 +239,8 @@ def main():
         df['embeddings'] = df['embeddings'].apply(lambda x: np.array(x, dtype=np.float128))
         df_norm['embeddings'] = df_norm['embeddings'].apply(lambda x: np.array(x, dtype=np.float128))
 
-    train_test_ratio = 0.8
-    df = df.sample(frac=1, random_state=1)
+    train_test_ratio = 0.7
+    df = df.sample(frac=1, **kwargs)
     df_norm = df_norm.sample(frac=1, random_state=1)
     train_df = df.iloc[:int(np.floor(train_test_ratio * len(df)))]
     test_df = df.iloc[int(np.floor(train_test_ratio * len(df))+1):]
@@ -278,25 +301,78 @@ def main():
     train_df_norm['pmf_predict'] = train_df_norm[pmf_cols].idxmax(axis=1).str.extract('(\d+)').astype(int)
     train_df_norm['pmf_correct'] = (train_df_norm['pmf_predict'] == train_df_norm['classid']).astype(int)
 
-    # Get incorrect predictions
-    incorrect_predictions = train_df[train_df['pmf_correct'] == 0]
-    incorrect_predictions_norm = train_df_norm[train_df_norm['pmf_correct'] == 0]
+    learning_rate = 1e-14
+    learning_step = 1000
+    for i in range(5): # Category Loop
+        for j in range(learning_step):
+            if j == 0:
+                prior_loss = 10000 # Some large number to initialize loss
+            loss_list = []
+            for k in range(len(test_df)):
+                loss = cost(set([test_df.iloc[k]['classid']]), set([i]) if test_df[f'pmf_cat{i}'][k] > category_threshold[i] else set([]))
+                loss_list.append(loss)
+            
+            loss_mean = np.mean(loss_list)  
+            if loss_mean < prior_loss:
+                category_threshold[i] += learning_rate
+            elif loss_mean > prior_loss:
+                category_threshold[i] -= learning_rate
+            else:
+                pass
+            prior_loss = loss_mean
+        
+    # Repeat for normalized data
+    for i in range(5): # Category Loop
+        for j in tqdm(range(learning_step)) :
+            if j == 0:
+                prior_loss = 10000
+            loss_list = []
+            for k in range(len(test_df_norm)):
+                loss = cost(set([test_df_norm.iloc[k]['classid']]), set([i]) if test_df_norm[f'pmf_cat{i}'][k] > category_threshold_norm[i] else set([]))
+                loss_list.append(loss)
+            
+            loss_mean = np.mean(loss_list)
+            if loss_mean < prior_loss:
+                category_threshold_norm[i] += learning_rate
+            elif loss_mean > prior_loss:
+                category_threshold_norm[i] -= learning_rate
+            else:
+                pass
+            prior_loss = loss_mean
 
-    # Check every category is contained in incorrect predictions
-    for i in range(5):
-        category_mask = incorrect_predictions['classid'] == i
-        category_threshold[i] = incorrect_predictions.loc[category_mask, f"pmf_cat{i}"].max()
-        category_mark_norm = incorrect_predictions_norm['classid'] == i
-        category_threshold_norm[i] = incorrect_predictions_norm.loc[category_mark_norm, f"pmf_cat{i}"].max()
     print(category_threshold)
-    print(category_threshold_norm)
 
-    for i in range(5):
-        test_df[f"candinates"] = test_df[pmf_cols].apply(lambda x: x[x > category_threshold[i]].index.tolist(), axis=1)
-        test_df_norm[f"candinates"] = test_df_norm[pmf_cols].apply(lambda x: x[x > category_threshold_norm[i]].index.tolist(), axis=1)
-    
-    print(test_df.head())
-    print(test_df_norm.head())
-    
+    test_df['candidate_category'] = test_df.apply(lambda x: x['pmf_predict'] if x[f"pmf_cat{x['pmf_predict']}"] >= category_threshold[x['pmf_predict']] else x['classid'], axis=1)
+    test_df_norm['candidate_category'] = test_df_norm.apply(lambda x: x['pmf_predict'] if x[f"pmf_cat{x['pmf_predict']}"] >= category_threshold_norm[x['pmf_predict']] else x['classid'], axis=1)
+
+    # PMF columns in your DataFrame
+    pmf_columns = [f'pmf_cat{i}' for i in range(5)]
+
+    # Apply the function to find candidates for each document
+    test_df['candidate_categories'] = test_df.apply(find_candidates, axis=1, pmf_cols=pmf_columns, thresholds=category_threshold)
+    test_df_norm['candidate_categories'] = test_df_norm.apply(find_candidates, axis=1, pmf_cols=pmf_columns, thresholds=category_threshold)
+
+    # Savezx
+    test_df.to_csv(os.path.join(root_path, 'test_df.csv'))
+    test_df_norm.to_csv(os.path.join(root_path, 'test_df_norm.csv'))
+    return test_df['pmf_correct'].mean(), test_df_norm['pmf_correct'].mean()
+
 if __name__ == "__main__":
-    main()
+    # test_size = 20
+    # accuracy = []
+    # accuracy_norm = []
+    # test_rand_array = [random.randint(0, 100) for i in range(test_size)]
+    # for seed in tqdm(test_rand_array):
+    #     _acc, _acc_norm = main(rand_seed=seed)
+    #     accuracy.append(_acc)
+    #     accuracy_norm.append(_acc_norm)
+    
+    # _df = pd.DataFrame({
+    #     'accuracy':accuracy,
+    #     'accuracy_norm':accuracy_norm
+    # })
+
+    # df_melt = _df.melt(var_name='Accuracy Type', value_name='Accuracy Value')
+    # sns.boxplot(x='Accuracy Type', y='Accuracy Value', data=df_melt)
+    # plt.show()
+    main(random_state=0)
